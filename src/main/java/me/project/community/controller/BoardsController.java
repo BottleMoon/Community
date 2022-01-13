d6e2cc4ba24191c54ba37c317f0d5ce4645b1fac
BottleMoon <gwonbj.dev@gmail.com>
2022-01-13 16:55:06 +0900

    Feat: BoardsController with RestController

---
+++ src/main/java/me/project/community/controller/BoardsController.java
@@ -0,0 +1,12 @@
+package me.project.community.controller;
+
+import me.project.community.dto.BoardDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BoardsController {
+
+}
